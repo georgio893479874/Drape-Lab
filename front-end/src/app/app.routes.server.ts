8f694f6cc86db2a43cedd604dc67f8b32bedd489
georgio893479874 <svipe83@gmail.com>
2026-07-23 13:54:57 +0300

    n

--- front-end/src/app/app.routes.server.ts
+++ front-end/src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'products/:id',
     renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
   },
 ];
